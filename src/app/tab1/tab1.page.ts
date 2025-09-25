b51335c8c2a9d633b5ad844fba6678330937a861
vala <vala@utb.cz>
2025-09-25 14:27:35 +0200

    Na tabu1 - základ UI + app. logika počítadla

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [FormsModule, IonInput, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
+  count = 0
+  name = ""
+
   constructor() {}
+
+  onIncrement() {
+    console.log('click')
+    console.log(this.name)
+    this.count++
+  }
+
+  onReset() {
+    console.log('reset')
+    this.count = 0
+  }
 }
